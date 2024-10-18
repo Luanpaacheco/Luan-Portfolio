8a26e163f01bd0308055f384ed92d74de9f88093
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 18:08:31 -0300

    feature: adicionando um espaço na assinatura la de cima

--- src/pages/Home/sections/navigateBar/NavBar.tsx
+++ src/pages/Home/sections/navigateBar/NavBar.tsx
@@ -8,8 +8,8 @@ const NavBar=()=> {
 
   return (
     <nav className="flex justify-between items-center px-10  bg-[#161513] text-white fixed w-full z-10 h-fit">
-            <img className='h-14 'src={signature}alt="" />
-            <ul className="flex gap-10 pr-28">
+            <img className='h-14 pt-1'src={signature}alt="" />
+            <ul className="flex gap-10 pr-28 " >
                 <li><a href="#home" className="text-sm hover:text-[#6148ac]">Home</a></li>
                 <li><a href="#aboutme" className="text-sm hover:text-[#6148ac]">About me</a></li>
                 <li><a href="#projects" className="text-sm hover:text-[#6148ac]">Projects</a></li>
