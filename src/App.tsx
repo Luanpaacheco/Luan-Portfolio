691ddbf366474e374fe46dd8040ae972b9bc2d6f
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 15:38:31 -0300

    feature: adicionando barra de rolagem horizontal

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App=()=> {
 
   return (
     <>
-      <div className="bg-[#161513]  w-full h-full font-sans">
+      <div className="bg-[#161513]  w-full h-full font-sans overflow-x-hidden">
         <Home/>
       </div>
       
