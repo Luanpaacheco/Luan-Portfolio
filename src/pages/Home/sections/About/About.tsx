0c00168bcb985cd76d3c75475b5711e9a08ec8f3
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 13:26:36 -0300

    feature:adicionando alguns detalhes

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -32,10 +32,9 @@ const About=()=> {
     <section id="aboutme">
       <div  ref={eelementoRef} className='translate-y-[500px] opacity-0 bg-[#0C0B0A] h-[24rem] rounded-[55px] text-center text-white flex justify-start align-top flex-col pt-14 mt-[3.5rem] '>
         <h1 className='text-4xl font-bold mb-4 text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>About Me</h1>
-        <p className='pt-5 pb-9 px-48 text-lg'>Certa vez, um pinguim resolveu abrir uma startup de sorvetes no Polo Sul. 
-            O slogan era "Mais gelado, só do lado de fora!" Ele achou que ia fazer sucesso, 
-            mas ninguém apareceu. Afinal, quem é que quer tomar sorvete onde já faz -30°C? Desolado, 
-            ele fechou a loja e decidiu investir em algo mais quente: virou vendedor de cobertores elétricos para focas.
+        <p className='pt-5 pb-9 px-48 text-lg'>I am a <span className=' font-bold  text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>full-stack developer</span>, 
+        a Software Engineering student at PUCRS in my third semester, 20 years old, and Brazilian. Driven by a passion for technology, 
+        I constantly seek opportunities to learn and challenge myself. In my free time, I enjoy spending time with <span className=' font-bold  text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>family and friends</span> and <span className=' font-bold  text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>practicing sports</span>.
         </p>
         <div className="  left-1/2 flex justify-center gap-12 pt-8 rounded-lg h-fit">
         <img className='h-8' src={programacao} />
