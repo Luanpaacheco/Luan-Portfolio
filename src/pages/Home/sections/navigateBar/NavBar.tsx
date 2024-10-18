cb28ed444cbe2a10d09e9061e670e4208f61feb3
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 01:01:02 -0300

    feture: ajustando o tamanho da NavBar

--- src/pages/Home/sections/navigateBar/NavBar.tsx
+++ src/pages/Home/sections/navigateBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import programacao from './codigo.png'
 import familia from './familia.png'
 import futebol from './futebol.png'
-
+import signature from '../../../../assets/signature.svg'
 
 
 
@@ -10,14 +10,14 @@ const NavBar=()=> {
    
 
   return (
-    <nav className="flex justify-between items-center px-10 py-5 bg-[#161513] text-white fixed w-full z-10">
-            <div className="text-2xl font-bold">Luan Pacheco</div>
+    <nav className="flex justify-between items-center px-10  bg-[#161513] text-white fixed w-full z-10 h-fit">
+            <img className='h-14 'src={signature}alt="" />
             <ul className="flex gap-10 pr-28">
-                <li><a href="#home" className="text-lg hover:text-[#6148ac]">Home</a></li>
-                <li><a href="#aboutme" className="text-lg hover:text-[#6148ac]">About me</a></li>
-                <li><a href="#projects" className="text-lg hover:text-[#6148ac]">Projects</a></li>
-                <li><a href="#experience" className="text-lg hover:text-[#6148ac]">Experience</a></li>
-                <li><a href="#contact" className="text-lg hover:text-[#6148ac]">Contact</a></li>
+                <li><a href="#home" className="text-sm hover:text-[#6148ac]">Home</a></li>
+                <li><a href="#aboutme" className="text-sm hover:text-[#6148ac]">About me</a></li>
+                <li><a href="#projects" className="text-sm hover:text-[#6148ac]">Projects</a></li>
+                <li><a href="#experience" className="text-sm hover:text-[#6148ac]">Experience</a></li>
+                <li><a href="#contact" className="text-sm hover:text-[#6148ac]">Contact</a></li>
             </ul>
         </nav>
   )
