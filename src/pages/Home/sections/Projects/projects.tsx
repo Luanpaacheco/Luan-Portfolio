27bd4b02dc68a824c066f0fdd23c5b8f447aebe3
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 01:01:49 -0300

    feature:Ajustando a animacao dos projects

--- src/pages/Home/sections/Projects/projects.tsx
+++ src/pages/Home/sections/Projects/projects.tsx
@@ -1,222 +1,72 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// // Import Swiper styles
-// import 'swiper/css';
-
-// import { EffectCards, Navigation, Pagination } from 'swiper/modules';
-
-// const Projects = () => {
-//   return (
-//     <div className='h-[40rem] items-center justify-center flex-col align-middle '>
-
-    
-//     <div className=''>
-//       <h2 className="text-center text-purple-500 mb-4">PROJECTS</h2>
-//       <div className='w-screen flex justify-center'>
-
-      
-//       <div className='w-2/4'>
-
-      
-//       <Swiper
-//          slidesPerView={3}
-//          spaceBetween={0}
-//          loop={true}
-//          pagination={{
-//            clickable: true,
-//          }}
-//          navigation={true}
-//          modules={[Pagination, Navigation]}
-//          className="mySwiper"
-//       >
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between drop-shadow-2xl`}>
-//             <div className="flex-1 rounded-t-lg bg-red-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-blue-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-green-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-orange-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-yellow-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-pink-600 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-green-800 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-//             <div className="flex-1 rounded-t-lg bg-blue-800 drop-shadow-2xl"></div>
-//             <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-//           </div>
-//         </SwiperSlide>
-//         <SwiperSlide>
-          // <div className={`bg-gray-700 rounded-lg h-40 w-72 flex flex-col justify-between`}>
-          //   <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-          //   <div className="flex items-center justify-between bg-slate-900 p-2 rounded-b-lg drop-shadow-2xl"></div>
-          // </div>
-//         </SwiperSlide>
-//       </Swiper>
-//     </div>
-//     </div>
-//     </div>
-//     </div>
-//   );
-// };
-
-// export default Projects;
-
-
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import './Projects.css';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
-import github from '../Hero/github.png'
-import linkedin from '../Hero/logotipo-do-linkedin.png'
+import html from '../../../../assets/html-5.png'
 
-import python from './python.png'
-import html from './html-5.png'
+const Projects: React.FC = () => {
+    const projects = [
+        {
+            image: html,
+            title: 'Calculadora',
+            link: 'https://github.com/gabrielahf/Calculadora',
+        },
+        {
+            image: html,
+            title: 'Conversor de Moedas',
+            link: 'https://github.com/gabrielahf/Conversor_Moedas',
+        },
+        {
+            image: 'Foto1',
+            title: 'Portfolio',
+            link: '#',
+        },
+    ];
 
-import {register} from 'swiper/element/bundle'
-import { motion } from 'framer-motion';
-register();
-// import { EffectCards, Navigation, Pagination } from 'swiper/modules';
+    return (
+        <>
+        <div>
+        
+        <div className=' pt-7'>
 
-const Projects = () => {
-
-  // const data={
-  //   {id:'1', ima},{}
-  // }
-  return (
-
-    <motion.section
-    
-    id="projects" 
-    initial={{ opacity: 0, y: 50 }} 
-    whileInView={{ opacity: 1, y: 0 }} 
-    transition={{ duration: 0.2 }} 
-    viewport={{ once: false }}>
-        <div className='py-28'>
-      <h1 className='text-4xl font-bold pb-9 text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>PROJECTS</h1>
-    
-    <Swiper
-      slidesPerView={3}
-      pagination={{clickable:true}}
-      navigation
-     className='w-2/4 flex justify-center align-middle items-center pb-10 ' 
-    >
-      <SwiperSlide className=''>
-        <div className=' flex justify-center align-middle items-center'>
-          <div className={`bg-gray-700 rounded-lg h-96 w-60 flex flex-col `}>
-            <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-            <div className="flex items-center gap-20 bg-slate-900 p-3 rounded-b-lg drop-shadow-2xl">
-            <button>
-            <img className='h-7 hover:scale-125 duration-300'src={github} alt="" />
-          </button>
-          <h2 className='text-white'>Instamovie</h2>
+        <h1 className='text-4xl font-bold text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>Projects</h1>
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className=""
+            
+            >
+                {projects.map((project, index) => (
+                    <SwiperSlide key={index}>
+                        {/* Envolvendo o card com o link */}
+                        <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
+                            <div className="slide-content">
+                                <img src={project.image} alt={project.title} className="project-image" />
+                                <h3>{project.title}</h3>
+                            </div>
+                        </a>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
             </div>
-          </div>
-        </div>
-      
-          {/* <img className='object-fill w-full h-52' src={python} alt="" /> */}
-      </SwiperSlide>
-      <SwiperSlide className=''>
-        <div className=' flex justify-center align-middle items-center'>
-          <div className={`bg-gray-700 rounded-lg h-96 w-60 flex flex-col `}>
-            <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-            <div className="flex items-center gap-20 bg-slate-900 p-3 rounded-b-lg drop-shadow-2xl">
-            <button>
-            <img className='h-7 hover:scale-125 duration-300'src={github} alt="" />
-          </button>
-          <h2 className='text-white'>Instamovie</h2>
             </div>
-          </div>
-        </div>
-      
-          {/* <img className='object-fill w-full h-52' src={python} alt="" /> */}
-      </SwiperSlide>
-      <SwiperSlide className=''>
-        <div className=' flex justify-center align-middle items-center'>
-          <div className={`bg-gray-700 rounded-lg h-96 w-60 flex flex-col `}>
-            <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-            <div className="flex items-center gap-20 bg-slate-900 p-3 rounded-b-lg drop-shadow-2xl">
-            <button>
-            <img className='h-7 hover:scale-125 duration-300'src={github} alt="" />
-          </button>
-          <h2 className='text-white'>Instamovie</h2>
-            </div>
-          </div>
-        </div>
-      
-          {/* <img className='object-fill w-full h-52' src={python} alt="" /> */}
-      </SwiperSlide>
-      <SwiperSlide className=''>
-        <div className=' flex justify-center align-middle items-center'>
-          <div className={`bg-gray-700 rounded-lg h-96 w-60 flex flex-col `}>
-            <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-            <div className="flex items-center gap-20 bg-slate-900 p-3 rounded-b-lg drop-shadow-2xl">
-            <button>
-            <img className='h-7 hover:scale-125 duration-300'src={github} alt="" />
-          </button>
-          <h2 className='text-white'>Instamovie</h2>
-            </div>
-          </div>
-        </div>
-      
-          {/* <img className='object-fill w-full h-52' src={python} alt="" /> */}
-      </SwiperSlide>
-      <SwiperSlide className=''>
-        <div className=' flex justify-center align-middle items-center'>
-          <div className={`bg-gray-700 rounded-lg h-96 w-60 flex flex-col `}>
-            <div className="flex-1 rounded-t-lg bg-purple-600 drop-shadow-2xl"></div>
-            <div className="flex items-center gap-20 bg-slate-900 p-3 rounded-b-lg drop-shadow-2xl">
-            <button>
-            <img className='h-7 hover:scale-125 duration-300'src={github} alt="" />
-          </button>
-          <h2 className='text-white'>Instamovie</h2>
-            </div>
-          </div>
-        </div>
-      
-          {/* <img className='object-fill w-full h-52' src={python} alt="" /> */}
-      </SwiperSlide>
-      
-    </Swiper>
-    </div>
-    </motion.section>
-    
-  );
+        </>
+    );
 };
 
-export default Projects;
-
-
+export default Projects;
