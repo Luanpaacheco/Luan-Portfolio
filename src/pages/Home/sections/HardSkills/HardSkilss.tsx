7157b77f77b7afffc9ea98559c6088f04d2f31a0
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-18 01:03:16 -0300

    feture:ajustando a disposicao do knowhow

--- src/pages/Home/sections/HardSkills/HardSkilss.tsx
+++ src/pages/Home/sections/HardSkills/HardSkilss.tsx
@@ -5,12 +5,15 @@ import react from '../Hero/biblioteca.png'
 import { motion } from 'framer-motion'
 import { div } from 'framer-motion/client'
 import java from "../../../../assets/javaC.png"
-import  Javascript  from '../../../../assets/javaC.png';
-import python from '../../../../assets/javaC.png'
-import css from '../../../../assets/javaC.png'
-import react from '../../../../assets/javaC.png'
-import html from '../../../../assets/javaC.png'
-import mongodb from '../../../../assets/javaC.png'
+import  Javascript  from '../../../../assets/script-java.png';
+import python from '../../../../assets/python.png'
+import css from '../../../../assets/css-3.png'
+import linux from '../../../../assets/plataforma-linux.png'
+import git from '../../../../assets/git.png'
+import typescript from '../../../../assets/typescript.png'
+import html from '../../../../assets/html-5.png'
+import mongodb from '../../../../assets/mongodb-2.png'
+import docker from '../../../../assets/docker.png'
 
 
 
@@ -35,26 +38,25 @@ const HardSkillss=()=> {
 //   <img src={react} alt="TypeScript" className="h-12 col-span-2" />
 // </div>
 
-<div className='bg-red-700 flex-row h-fit justify-center items-center align-middle'>
+<div className=' flex-row h-fit justify-center items-center align-middle my-32'>
 <h1 className='text-4xl font-bold mb-4 text-center bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>Know-How</h1>
     <div className=' h-fit'>
-    <div className="flex justify-center space-x-6 ">
-  <img src={react} alt="Java" className="h-12" />
-  <img src={react} alt="JavaScript" className="h-12" />
-  <img src={react} alt="Python" className="h-12" />
-  <img src={react} alt="CSS" className="h-12" />
-  <img src={react} alt="HTML" className="h-12" />
-  <img src={react} alt="Python" className="h-12" />
-  <img src={react} alt="CSS" className="h-12" />
-  <img src={react} alt="HTML" className="h-12" />
+    <div className="flex justify-center space-x-10 pt-7">
+  <img src={java} alt="Java" className="h-20" />
+  <img src={Javascript} alt="JavaScript" className="h-20" />
+  <img src={python} alt="Python" className="h-20" />
+  <img src={css} alt="CSS" className="h-20" />
+  <img src={html} alt="HTML" className="h-20" />
+  <img src={react} alt="Python" className="h-20" />
+  <img src={mongodb} alt="CSS" className="h-20" />
     </div>
-    <div className=' h-fit'>
-    <div className="flex justify-center space-x-6  ">
-    <img src={react} alt="Java" className="h-12" />
-  <img src={react} alt="JavaScript" className="h-12" />
-  <img src={react} alt="Python" className="h-12" />
-  <img src={react} alt="CSS" className="h-12" />
-  <img src={react} alt="HTML" className="h-12" />
+    <div className=' h-fit pt-8'>
+    <div className="flex justify-center space-x-10  ">
+    <img src={react} alt="Java" className="h-20" />
+    <img src={linux} alt="JavaScript" className="h-20" />
+    <img src={git} alt="Python" className="h-20" />
+    <img src={typescript} alt="CSS" className="h-20" />
+    <img src={docker} alt="CSS" className="h-20" />
     </div>
     </div>
 </div>
