d21d41af5125f79822198b5ab32fc32ae5bba93f
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-10-26 16:44:51 -0300

    ajustando o ingles

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -27,9 +27,8 @@ const About = () => {
           About Me
         </h1>
         <p className='pt-9 pb-5 md:pb-5 lg:pb-9 px-6 sm:px-20 md:px-48 text-base sm:text-lg'>
-          I am a <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>full-stack developer</span>, 
-          a Software Engineering student at PUCRS in my third semester, 20 years old, and Brazilian. Driven by a passion for technology, 
-          I constantly seek opportunities to learn and challenge myself. In my free time, I enjoy spending time with <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>family and friends</span> and <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>practicing sports</span>.
+          I am a Software Engineering student in my third semester  at PUCRS, and <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>a full-stack developer</span>, 
+          . A 20-year-old Brazilian driven by the passion for technology, and constantly seeking opportunities to learn and challenge myself. In my free time, I enjoy spending time with my <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>family and friends</span>, and <span className='font-bold bg-gradient-to-r from-[#6148ac] to-[#6312d6] bg-clip-text text-transparent'>practicing sports</span>.
         </p>
         <div className="flex flex-wrap justify-center gap-6 sm:gap-12 lg:pt-8 md:pt-0 rounded-lg h-fit mb-6 sm:mb-0">
           <img className='h-8' src={programacao} alt="Programming" />
